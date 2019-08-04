d82076deaac6cb49ab1a44f49cfd66d6a0d78f7d
Jimmy Miller <jimmyhmiller@gmail.com>
2019-08-04 12:38:34 -0400

    New Build

---
+++ _next/static/webpack/static/development/pages/index.js.eb7d9392da8e77b71ebc.hot-update.js
@@ -0,0 +1,145 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _utils_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../utils.js */ "./utils.js");
+var _jsxFileName = "/Users/jimmyhmiller/Documents/Code/jimmyhmiller.github.io/pages/index.js";
+
+
+var posts = [{
+  text: "Variants Explained",
+  href: "/variants-explained"
+}, {
+  text: "Variants and Protocols",
+  href: "/variants-and-protocols"
+}, {
+  text: "Protomorphism",
+  href: "/protomorphism"
+}, {
+  text: "Beautiful Code Through Simplicity",
+  href: "/beautiful-code"
+}, {
+  text: "OOP from the Ground Up",
+  href: "/oop-ground-up"
+}, {
+  text: "Side Effects, Complecting a la Carte",
+  href: "/side-effects-complect"
+}, {
+  text: "Basic Functional Studies",
+  href: "/basic-functional-studies"
+}, {
+  text: "Defending the Incommunicability of Programs",
+  href: "/incommunicability"
+}, {
+  text: "Named Function Composition",
+  href: "/named-function-composition"
+}, {
+  text: "Meander for Practical Data Transformation",
+  href: "/meander-practical"
+}, {
+  text: "Term Rewriting with Meander",
+  href: "/meander-rewriting"
+}];
+var utilities = [{
+  text: "Graph Maker",
+  href: "https://jimmyhmiller.github.io/graph-maker/"
+}, {
+  text: "Finite State Machine Maker",
+  href: "https://jimmyhmiller.github.io/fsm-maker/"
+}, {
+  text: "EsLint Fixit",
+  href: "https://github.com/jimmyhmiller/eslint-fixit"
+}, {
+  text: "Zoom Launcher",
+  href: "https://github.com/jimmyhmiller/zoom-cli"
+}];
+var libraries = [{
+  text: "MultiMethods in Javascript",
+  href: "https://github.com/jimmyhmiller/multiple-methods"
+}, {
+  text: "React Redux Connected",
+  href: "https://github.com/jimmyhmiller/react-redux-connected"
+}];
+var slides = [{
+  text: "The Future of Programming",
+  href: "https://future-of-programming.now.sh"
+}, {
+  text: "What is a Monad?",
+  href: "https://what-is-a-monad.now.sh"
+}, {
+  text: "Practical Functional Refactoring",
+  href: "https://practical-functional-refactoring.now.sh"
+}, {
+  text: "Property Based Testing",
+  href: "https://generative-testing.now.sh"
+}, {
+  text: "Datalog Lightning Talk",
+  href: "https://datalog.now.sh"
+}];
+var project = [{
+  text: "One Hundred Lines or Less",
+  href: "https://github.com/jimmyhmiller/one-hundred-lines-or-less"
+}];
+/* harmony default export */ __webpack_exports__["default"] = (function () {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_utils_js__WEBPACK_IMPORTED_MODULE_1__["GlobalLayout"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 116
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_utils_js__WEBPACK_IMPORTED_MODULE_1__["LinkList"], {
+    title: "Posts",
+    items: posts,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 117
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_utils_js__WEBPACK_IMPORTED_MODULE_1__["LinkList"], {
+    title: "Utilities",
+    items: utilities,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 121
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_utils_js__WEBPACK_IMPORTED_MODULE_1__["LinkList"], {
+    title: "Libraries",
+    items: libraries,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 125
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_utils_js__WEBPACK_IMPORTED_MODULE_1__["LinkList"], {
+    title: "Slides",
+    items: slides,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 129
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_utils_js__WEBPACK_IMPORTED_MODULE_1__["LinkList"], {
+    title: "Projects",
+    items: project,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 133
+    },
+    __self: this
+  }));
+});
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.eb7d9392da8e77b71ebc.hot-update.js.map
