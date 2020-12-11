63167f0f1de70547d9bc2f25cf596592deb1dec9
Jimmy Miller <jimmyhmiller@adzerk.com>
2020-12-10 19:09:52 -0500

    New Build

--- _next/static/runtime/main.js
+++ _next/static/runtime/main.js
@@ -6187,7 +6187,7 @@ function connect(options) {
       error.name = err.name;
       error.stack = err.stack; // __NEXT_DIST_DIR is provided by webpack
 
-      (0, _sourceMapSupport.rewriteStacktrace)(error, "/Users/jimmyhmiller/Documents/Code/jimmyhmiller.github.io/.next");
+      (0, _sourceMapSupport.rewriteStacktrace)(error, "/Users/jimmy/Documents/Code/jimmyhmiller.github.io/.next");
       return error;
     }
   };
