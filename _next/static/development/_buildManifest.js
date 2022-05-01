99c8518aa550ef26a07aaa416d9703a541ca7992
Jimmy Miller <jimmyhmiller@gmail.com>
2022-05-01 11:39:26 -0400

    New Build

--- _next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/aesthetic-elements":["static\u002Fchunks\u002Fpages\u002Faesthetic-elements.js"],sortedPages:["\u002F_app","\u002F_error","\u002Faesthetic-elements"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
