6f74a1d7ca6e739985d25cebdfc8cac404e07df9
Jimmy Miller <jimmyhmiller@gmail.com>
2022-06-28 18:22:32 -0400

    add placeholder images

---
+++ pages/yjit.js
@@ -0,0 +1,17 @@
+import { Heading, BlockQuote, Term, GlobalLayout, Title, Image } from "../utils.js";
+
+const YJit = () => (
+    <GlobalLayout>
+        <Title text="YJIT" />
+        <p>Todo: Write out text</p>
+
+        <Image src="images/intro.gif" />
+        <Image src="images/two.png" />
+        <Image src="images/three.gif" />
+        <Image src="images/full.png" />
+
+    </GlobalLayout>
+
+)
+
+export default YJit
