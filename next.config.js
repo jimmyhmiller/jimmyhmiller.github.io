abec5773133b96c1986652eee6667b991d4e81d6
Jimmy Miller <jimmyhmiller@gmail.com>
2023-12-27 12:38:10 -0500

    I love when software makese breaking changes like this for seemingly no reason

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const withMDX = require('@next/mdx')()
  
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   // Configure `pageExtensions` to include MDX files
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   // Optionally, add any other Next.js config below
